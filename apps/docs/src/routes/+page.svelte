bf4cc95492790bfa6cc9400f5d9ae161ad05f1e5
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2022-11-03 12:12:55 +0000

    Added autoplaying video

--- apps/docs/src/routes/+page.svelte
+++ apps/docs/src/routes/+page.svelte
@@ -1,8 +1,33 @@
-<script lang="ts">
-  import { MyCounterButton } from 'ui';
+<script lang="ts" src="https://player.vimeo.com/api/player.js">
 </script>
 
-<h1>Docs</h1>
-<MyCounterButton />
+<!-- src="https://player.vimeo.com/video/766852849?h=66d05c3eb9&badge=0&autopause=0&autoplay=1&loop=1&player_id=0&app_id=58479" -->
+<!-- <div style="padding:56.25% 0 0 0;position:relative;">
+  <iframe
+    src="https://player.vimeo.com/video/766852849?autoplay=1&loop=1&autopause=0"
+    frameborder="0"
+    allow="autoplay; fullscreen; picture-in-picture"
+    style="position:absolute;top:0;left:0;width:100%;height:100%;"
+    title="hlustunarp_03"></iframe>
+</div> -->
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <div style="padding:56.25% 0 0 0;position:relative;">
+  <iframe
+    src="https://player.vimeo.com/video/766852849?h=66d05c3eb9&badge=0&autopause=0&player_id=0&app_id=58479"
+    frameborder="0"
+    allow="autoplay; fullscreen; picture-in-picture"
+    allowfullscreen
+    style="position:absolute;top:0;left:0;width:100%;height:100%;"
+    title="hlustunarp_03"></iframe>
+</div> -->
+
+<div style="padding:56.25% 0 0 0;position:relative;">
+  <iframe
+    src="https://player.vimeo.com/video/766852849?background=1?h=66d05c3eb9&badge=0&autopause=0&autoplay=1&player_id=0&app_id=58479"
+    frameborder="0"
+    allow="autoplay; fullscreen; picture-in-picture"
+    allowfullscreen
+    data-ready="true"
+    style="position:absolute;top:0;left:0;width:100%;height:100%;"
+    title="hlustunarp_03"></iframe>
+</div>
